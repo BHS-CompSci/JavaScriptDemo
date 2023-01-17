a483f1bf37c12765adef1661c6c497845a5a53f3
Carl Walther <carl.walther@sbcglobal.net>
2023-01-17 10:03:38 -0600

    Updates During Class
    Updates During Class

---
+++ Code/math.js
@@ -0,0 +1,12 @@
+let answerElement = document.getElementById("answer");
+let equationElement = document.getElementById("equation");
+
+let a = 7;
+let b = 3;
+let c = 4;
+let question = a += 2;
+let test = a;
+answerElement.textContent = test;
+equationElement.textContent = question;
+
+
